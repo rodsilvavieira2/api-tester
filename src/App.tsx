04c867597d55048aca344dcade764801cf4c5a46
Rodrigo silva <rodsilvasoul@gmail.com>
2022-01-22 13:32:23 -0400

    chore: initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box } from '@chakra-ui/react'
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Box w="100%" minH="5" bg="red">
+      test
+    </Box>
+  )
 }
-
-export default App;
