4c019d3e65ec48224d088a6a69cf64ec09f7f38e
Rodrigo silva <rodsilvasoul@gmail.com>
2022-01-28 09:02:27 -0400

    feat(search-input.ts): new component searchInput

---
+++ src/components/molecules/search-input.tsx
@@ -0,0 +1,25 @@
+import { forwardRef, ForwardRefRenderFunction } from 'react'
+import { MdOutlineSearch } from 'react-icons/md'
+
+import {
+  IconButton,
+  Input,
+  InputGroup,
+  InputProps,
+  InputRightElement
+} from '@chakra-ui/react'
+
+const Base:ForwardRefRenderFunction<HTMLInputElement, InputProps> = (props, ref) => {
+  return (
+    <InputGroup>
+      <Input {...props} ref={ref}/>
+      <InputRightElement>
+        <IconButton aria-label="pesquisas">
+          <MdOutlineSearch />
+        </IconButton>
+      </InputRightElement>
+    </InputGroup>
+  )
+}
+
+export const SearchInput = forwardRef(Base)
