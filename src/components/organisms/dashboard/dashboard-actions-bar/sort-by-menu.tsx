8169fd076f25dd97aba15c0d90d553f98593be83
Rodrigo silva <rodsilvasoul@gmail.com>
2022-01-28 09:07:18 -0400

    feat(sort-by-menu.tsx): new component SortByMenu

---
+++ src/components/organisms/dashboard/dashboard-actions-bar/sort-by-menu.tsx
@@ -0,0 +1,45 @@
+import { AiOutlineSortAscending } from 'react-icons/ai'
+import { TiArrowUnsorted } from 'react-icons/ti'
+import { useDispatch, useSelector } from 'react-redux'
+
+import {
+  Menu,
+  MenuButton,
+  IconButton,
+  MenuList,
+  MenuOptionGroup,
+  MenuItemOption,
+  HStack,
+  Text,
+  Icon
+} from '@chakra-ui/react'
+
+import { SortBy } from '../../../../@types'
+import { selectSortBy, setSortBy } from '../../../../redux/slices'
+
+export const SortByMenu = () => {
+  const sortBy = useSelector(selectSortBy)
+
+  const appDispatch = useDispatch()
+
+  return (
+    <Menu>
+      <MenuButton as={IconButton} icon={<TiArrowUnsorted />} />
+
+      <MenuList>
+        <MenuOptionGroup
+          defaultValue={sortBy}
+          onChange={(e) => appDispatch(setSortBy(e as SortBy))}
+        >
+          <MenuItemOption value="newest">Mais recentes</MenuItemOption>
+
+          <MenuItemOption value="oldest">Mais antigos</MenuItemOption>
+
+          <MenuItemOption value="ascending">Crescente</MenuItemOption>
+
+          <MenuItemOption value="descending">Decrescente (Z-a)</MenuItemOption>
+        </MenuOptionGroup>
+      </MenuList>
+    </Menu>
+  )
+}
